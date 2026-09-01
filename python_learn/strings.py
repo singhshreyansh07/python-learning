56bb65364a54105b3db1b39c58c73ce1ec74725e
singhshreyansh07 <shreyanshthakur1415@gmail.com>
2026-09-02 00:58:28 +0530

    easy concept

--- python_learn/strings.py
+++ python_learn/strings.py
@@ -6,3 +6,17 @@ My name is Shreyansh Singh.
 I don't ever give up.
 I always attract what i want.
 """
+
+first_name = "Shreyansh"
+last_name = "Singh"
+
+#printed full name but without space.
+#full_name = first_name + last_name
+
+#printing with space. easy concept
+full_name = first_name + " " + last_name
+
+
+
+#Lets play
+dash = "-" * 10000
