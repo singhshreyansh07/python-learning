73770f04333db51ab66bbb8cb5c85f926f74891f
singhshreyansh07 <shreyanshthakur1415@gmail.com>
2026-09-04 16:45:22 +0530

    learned for loops

---
+++ python_learn/loops.py
@@ -0,0 +1,19 @@
+#will print output 0,1,2,3,4
+#remeber it will count from 0 bcz its zero indexing lang
+
+for i in range (5):
+    print(i)
+
+
+for i in range (8):
+    print("Hello World")
+
+
+#Lets print 1 to 5
+
+for i in range (1,6):
+    print(i)
+
+#Count by 2
+for i in range (0,10,2):
+    print(i)
